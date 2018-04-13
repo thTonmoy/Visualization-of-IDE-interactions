586fab149047d20e9f80077a90be98c4bc6f5b70
Live-Long <33460023+Live-Long@users.noreply.github.com>
2018-04-13 22:31:02 +0600

    more changed to netgraph
    more changed to netgraph, still yet to be completed

--- netGraph.py
+++ netGraph.py
@@ -8,15 +8,50 @@ import networkx as nx
 import csv
 from mpl_toolkits.mplot3d import Axes3D
 
+import plotly.plotly as py
+from plotly.graph_objs import *
+import warnings
 
-hackerRank_numericMapping = pd.read_csv('data/HackerRank-Developer-Survey-2018-Numeric-Mapping.csv')
+# Input data files check
+from subprocess import check_output
 
-netGraph = hackerRank_numericMapping
-netGraph.plot()
+#print(check_output(["ls", "../input"]).decode("utf8"))
 
-plt.xlabel ('Occupation')
-plt.ylabel ('Number of people')
-plt.title("Network Graph")
-plt.legend()
 
+
+warnings.filterwarnings('ignore')
+
+G = nx.Graph(day="Stackoverflow")
+df_nodes = pd.read_csv('data/HackerRank-Developer-Survey-2018-Numeric-Mapping.csv')
+df_edges = pd.read_csv('data/HackerRank-Developer-Survey-2018-Numeric-Mapping.csv')
+
+for index, row in df_nodes.iterrows():
+    G.add_node(row['name'], group=row['group'], nodesize=row['nodesize'])
+
+for index, row in df_edges.iterrows():
+    G.add_weighted_edges_from([(row['source'], row['target'], row['value'])])
+
+color_map = {1: '#f09494', 2: '#eebcbc', 3: '#72bbd0', 4: '#91f0a1', 5: '#629fff', 6: '#bcc2f2',
+             7: '#eebcbc', 8: '#f1f0c0', 9: '#d2ffe7', 10: '#caf3a6', 11: '#ffdf55', 12: '#ef77aa',
+             13: '#d6dcff', 14: '#d2f5f0'}
+
+plt.figure(figsize=(25, 25))
+options = {
+    'edge_color': '#FFDEA2',
+    'width': 1,
+    'with_labels': True,
+    'font_weight': 'regular',
+}
+colors = [color_map[G.node[node]['group']] for node in G]
+sizes = [G.node[node]['nodesize'] * 10 for node in G]
+
+"""
+Using the spring layout : 
+- k controls the distance between the nodes and varies between 0 and 1
+- iterations is the number of times simulated annealing is run
+default k=0.1 and iterations=50
+"""
+nx.draw(G, node_color=colors, node_size=sizes, pos=nx.spring_layout(G, k=0.25, iterations=50), **options)
+ax = plt.gca()
+ax.collections[0].set_edgecolor("#555555")
 plt.show()
