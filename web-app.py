6e465d8f9b8dd796786de1af8a229770947ac53e
thTonmoy <thTonmoy@users.noreply.github.com>
2018-04-18 23:30:28 +0600

    fixed errors

--- web-app.py
+++ web-app.py
@@ -60,41 +60,23 @@ def show_quiz():
     return render_template('quizHTML.html', q=questions_shuffled, o=questions)
 
 
-# @app.route('/quizAns', methods=['POST'])
-# def show_quiz_answers():
-#     from quiz import questions, original_questions
-#     from flask import request
-#     correct_amount = 0
-#     correct_answers = ''
-#     next = ',  \n'
-#
-#
-#     for i in list(questions.keys()):
-#         answered = request.form[i]
-#         if original_questions[i][0] == answered:
-#             correct_amount = correct_amount + 1
-#             correct_answers = correct_answers + answered + next
-#
-#     return render_template('quizAnswers.html', cor_amount=correct_amount, cor_answers=correct_answers)
-
-@app.route('/quizAns', methods='POST')
+@app.route('/quizAns', methods=['POST'])
 def show_quiz_answers():
-     from quiz import questions, original_questions
-     from flask import request
-     correct = 0
+    from quiz import questions, original_questions
+    from flask import request
+    correct_amount = 0
+    response = ''
 
-     response = '\n'
-     for i in list(questions.keys()):
-         answered = request.form[i]
-         response = response + '\nQuestion: ' + i + "\n" + 'Your answer ' + answered + '\n'
-         if original_questions[i][0] == answered:
-             correct = correct + 1
-             response = response + "is correct" + '\n'
-         else:
-             response = response + "is incorrect" + '\n'
+    for i in list(questions.keys()):
+        answered = request.form[i]
+        response = response + '\nQuestion: ' + i + "\n" + 'Your answer ' + answered + '\n'
+        if original_questions[i][0] == answered:
+            correct_amount = correct_amount + 1
+            response = response + "Correct" + '\n'
+        else:
+            response = response + "Incorrect, correct answer: " + original_questions[i][0]+ '\n'
 
-     return '<h1>' + str(correct) + '<pre>' + ' Correct answers:' + str(response) + '</pre' +  '</h>'
-
+    return render_template('quizAnswers.html', cor_amount=correct_amount, cor_answers=response.split('\n'))
 
 
 @app.route('/cluster')
@@ -105,5 +87,5 @@ def show_cluster():
 
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    app.run()
+    app.run(debug=True)
+    #app.run()
